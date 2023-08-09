16580bb66efdf0f434998295d1fc71ea3fc59aa8
soowan95 <ksw9565@gmail.com>
2023-08-09 17:50:03 +0900

    2023.08.09

--- html 공부/javaScript/nav.js
+++ html 공부/javaScript/nav.js
@@ -1,9 +1,10 @@
 /* javaScript */
 window.onload = function() {
-  
+
 }
 $(function() {
   $(".btn").on("click", function() {
+  // ---------------구구단
   let i = 0, j = 0;
   while(i < 9) {
     i++;
@@ -13,5 +14,12 @@ $(function() {
     }
     j = 0;
   }
-  })
+  // ------------짝수만 더하기
+  let k = sum = 0;
+  while(k < 50) {
+    k++;
+    sum += k * 2;
+  }
+  console.log(sum);
+  })  
 })

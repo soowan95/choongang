510886bce313e1b5b695405b8e621322f68db176
soowan95 <ksw9565@gmail.com>
2023-08-28 15:52:16 +0900

    2023.08.28

--- java_study/java20230828/src/ch06Class/C01Class.java
+++ java_study/java20230828/src/ch06Class/C01Class.java
@@ -4,10 +4,17 @@ public class C01Class {
   public static void main(String[] args) {
     MyClass01 o1 = new MyClass01();
     System.out.println(System.identityHashCode(o1));
-    
+
+    MyClass01 o2 = o1;
+
+    System.out.println(o2.age);
+
+    o2.age = 50;
+
+    System.out.println(o1.age);
   }
 }
 
 class MyClass01 {
-
+  int age = 30;
 }

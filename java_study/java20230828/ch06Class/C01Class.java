5f1b7d12a8a603328142ecf7f39ae3033a74c9a9
soowan95 <ksw9565@gmail.com>
2023-08-28 15:34:09 +0900

    2023.08.28

---
+++ java_study/java20230828/ch06Class/C01Class.java
@@ -0,0 +1,13 @@
+package ch06Class;
+
+public class C01Class {
+  public static void main(String[] args) {
+    MyClass01 o1 = new MyClass01();
+    System.out.println(System.identityHashCode(o1));
+    
+  }
+}
+
+class MyClass01 {
+
+}

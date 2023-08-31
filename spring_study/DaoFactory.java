8eefc94bbf11f5372537beeef3c3759bf3660c24
soowan95 <ksw9565@gmail.com>
2023-08-31 17:07:07 +0900

    use spring

--- spring_study/DaoFactory.java
+++ spring_study/DaoFactory.java
@@ -1,11 +1,14 @@
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DaoFactory {
-//  @Configuration
+  @Bean
   public UserDao userDao() {
     return new UserDao(connectionMaker());
   }
 
+  @Bean
   public ConnectionMaker connectionMaker() {
     return new DConnectionMaker();
   }
--- spring_study/UserDaoTest.java
+++ spring_study/UserDaoTest.java
@@ -1,8 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.sql.SQLException;
 
 public class UserDaoTest {
   public static void main(String[] args) throws ClassNotFoundException, SQLException {
-    UserDao dao = new DaoFactory().userDao();
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    UserDao dao = context.getBean("userDao", UserDao.class);
 
     User user = new User();
     user.setId("whiteship");

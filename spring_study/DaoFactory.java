ca6929e41fe9308dce06694c9ae65889c388748e
soowan95 <ksw9565@gmail.com>
2023-08-31 09:29:31 +0900

    add spring.jar

--- spring_study/DaoFactory.java
+++ spring_study/DaoFactory.java
@@ -1,4 +1,7 @@
+import org.springframework.context.annotation.Configuration;
+
 public class DaoFactory {
+//  @Configuration
   public UserDao userDao() {
     return new UserDao(connectionMaker());
   }

1aaa8fb2b4561820c52f908fea5c82515c97f6dc
soowan95 <ksw9565@gmail.com>
2023-09-01 14:16:50 +0900

    learn nest

--- java_study/java20230828/src/ch09Nested/lecture/C01Nested.java
+++ java_study/java20230828/src/ch09Nested/lecture/C01Nested.java
@@ -1,6 +1,11 @@
 package ch09Nested.lecture;
 
 public class C01Nested {
+  public static void main(String[] args) {
+//    MyClass01.NestedClass01 o1 = new MyClass01.NestedClass01;
+    MyClass01 o1 = new MyClass01();
+    MyClass01.NestedClass01 o2 = o1.new NestedClass01();
+  }
 }
 
 class MyClass01 {

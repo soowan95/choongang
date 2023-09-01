f52b8a0ee63ab77e19887fa72bdaa2910510e7d4
soowan95 <ksw9565@gmail.com>
2023-09-01 15:45:42 +0900

    learn access of static

---
+++ java_study/java20230828/src/ch09Nested/lecture/C05Access.java
@@ -0,0 +1,20 @@
+package ch09Nested.lecture;
+
+public class C05Access {
+   String name = "java";
+  class InnerClass {
+    String name = "spring";
+
+    void method() {
+      System.out.println(name);
+      System.out.println(this.name);
+      System.out.println(C05Access.this.name);
+    }
+  }
+
+  public static void main(String[] args) {
+    C05Access o2 = new C05Access();
+    InnerClass o1 = o2.new InnerClass();
+    o1.method();
+  }
+}

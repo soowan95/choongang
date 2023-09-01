71c4accc4e9eb6816616df531db27c1d505f515c
soowan95 <ksw9565@gmail.com>
2023-09-01 15:19:44 +0900

    learn import static

---
+++ java_study/java20230828/src/ch09Nested/lecture/C03ImportStatic.java
@@ -0,0 +1,13 @@
+package ch09Nested.lecture;
+
+import static java.lang.Math.*;
+
+public class C03ImportStatic {
+  public static void main(String[] args) {
+    double random = random();
+    double random1 = random();
+    double random2 = random();
+
+    double length = Math.PI * 3 * 2;
+  }
+}

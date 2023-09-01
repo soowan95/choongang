c3fb8662df605147a8d8902cd7ebb25e5ba68e17
soowan95 <ksw9565@gmail.com>
2023-09-01 09:27:32 +0900

    p416 Example8

---
+++ java_study/java20230828/src/ch08Interface/book/exercise/p416/No8/A.java
@@ -0,0 +1,5 @@
+package ch08Interface.book.exercise.p416.No8;
+
+public interface A {
+  void method1();
+}
---
+++ java_study/java20230828/src/ch08Interface/book/exercise/p416/No8/B.java
@@ -0,0 +1,8 @@
+package ch08Interface.book.exercise.p416.No8;
+
+public class B implements A {
+  @Override
+  public void method1() {
+    System.out.println("B.method1");
+  }
+}

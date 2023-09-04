d16b5e365bb11332e856e61dc5e78994429b5f51
soowan95 <ksw9565@gmail.com>
2023-09-04 10:35:28 +0900

    example anonymous class

---
+++ java_study/java20230828/src/ch09Nested/book/ch09/sec07/exam2/HomeExample.java
@@ -0,0 +1,23 @@
+package ch09Nested.book.ch09.sec07.exam2;
+
+public class HomeExample {
+  public static void main(String[] args) {
+    Home home = new Home();
+
+    home.use1();
+
+    home.use2();
+
+    home.use3(new RemoteControl() {
+      @Override
+      public void turnOn() {
+        System.out.println("난방을 켭니다.");
+      }
+
+      @Override
+      public void trunOff() {
+        System.out.println("난방을 끕니다.");
+      }
+    });
+  }
+}
---
+++ java_study/java20230828/src/ch09Nested/book/ch09/sec07/exam2/RemoteControl.java
@@ -0,0 +1,6 @@
+package ch09Nested.book.ch09.sec07.exam2;
+
+public interface RemoteControl {
+  void turnOn();
+  void trunOff();
+}

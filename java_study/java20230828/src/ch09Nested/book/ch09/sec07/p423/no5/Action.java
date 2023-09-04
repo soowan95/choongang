9e0e80febf8b1afdec92dd489f139f8889e5a1ae
soowan95 <ksw9565@gmail.com>
2023-09-04 11:19:58 +0900

    practice anonymous class

---
+++ java_study/java20230828/src/ch09Nested/book/ch09/sec07/p423/no5/Action.java
@@ -0,0 +1,5 @@
+package ch09Nested.book.ch09.sec07.p423.no5;
+
+public interface Action {
+  public void work();
+}
---
+++ java_study/java20230828/src/ch09Nested/book/ch09/sec07/p423/no6/Vehicle.java
@@ -0,0 +1,5 @@
+package ch09Nested.book.ch09.sec07.p423.no6;
+
+public interface Vehicle {
+  public void run();
+}

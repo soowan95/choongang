7124235dc5947771bea397496af88d63999da794
soowan95 <ksw9565@gmail.com>
2023-09-06 12:15:02 +0900

    learn unicode

---
+++ java_study/java20230828/src/ch12Api/lecture/C190Hangul.java
@@ -0,0 +1,21 @@
+package ch12Api.lecture;
+
+public class C190Hangul {
+  public static void main(String[] args) {
+    // 자바 문자 : unicode
+    char a = '\u0d05';
+    System.out.println(a);
+    System.out.println('\u0041');
+
+    System.out.println('\uac00');
+    System.out.println('\ud7a3');
+
+    System.out.println("손흥민".matches("[\uac00-\ud7a3]{3}")); // true
+    System.out.println("이강인".matches("[\uac00-\ud7a3]{3}")); // true
+    System.out.println("해리케인".matches("[\uac00-\ud7a3]{3}")); // false
+
+    System.out.println("손흥민".matches("[가-힣]{3}")); // true
+    System.out.println("이강인".matches("[가-힣]{3}")); // true
+    System.out.println("해리케인".matches("[가-힣]{3}")); // false
+  }
+}

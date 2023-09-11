be68c28d4270a7fca658b9c585424b1e2929f4bb
soowan95 <ksw9565@gmail.com>
2023-09-11 09:23:35 +0900

    learn iterator

---
+++ java_study/java20230828/src/ch15Collection/book/sec03/exam03.java
@@ -0,0 +1,37 @@
+package ch15Collection.book.sec03;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class exam03 {
+  public static void main(String[] args) {
+    Set<String> set = new HashSet<>();
+
+    set.add("java");
+    set.add("jdbc");
+    set.add("jsp");
+    set.add("spring");
+
+    Iterator<String> iterator = set.iterator();
+
+/*    String next = iterator.next();
+    System.out.println("next = " + next);
+    String next1 = iterator.next();
+    System.out.println("next1 = " + next1);
+    String next2 = iterator.next();
+    System.out.println("next2 = " + next2);
+    String next3 = iterator.next();
+    System.out.println("next3 = " + next3);*/
+//    iterator.next(); // exception
+
+    while(iterator.hasNext()) {
+      String next = iterator.next();
+      System.out.println("next = " + next);
+    }
+
+    for(String str : set) {
+      System.out.println("str = " + str);
+    }
+  }
+}

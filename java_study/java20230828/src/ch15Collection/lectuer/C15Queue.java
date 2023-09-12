15ffcc2924a6a20efec453b10b916af1d27c439c
soowan95 <ksw9565@gmail.com>
2023-09-12 12:36:25 +0900

    learn queue

---
+++ java_study/java20230828/src/ch15Collection/lectuer/C15Queue.java
@@ -0,0 +1,31 @@
+package ch15Collection.lectuer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class C15Queue {
+  public static void main(String[] args) {
+    // queue : FIFO(First In First Out), 선입선출
+    // 주요 메소드
+    // offer : 아이템 추가
+    // poll : 아이템 꺼내기
+
+    Queue<Integer> queue = new LinkedList<>();
+    queue.offer(100);
+    queue.offer(200);
+    queue.offer(300);
+
+
+    System.out.println("queue.size() = " + queue.size());
+    Integer item1 = queue.poll();
+    System.out.println("queue.size() = " + queue.size());
+    Integer item2 = queue.poll();
+    System.out.println("queue.size() = " + queue.size());
+    Integer item3 = queue.poll();
+    System.out.println("queue.size() = " + queue.size());
+
+    System.out.println("item1 = " + item1);
+    System.out.println("item2 = " + item2);
+    System.out.println("item3 = " + item3);
+  }
+}

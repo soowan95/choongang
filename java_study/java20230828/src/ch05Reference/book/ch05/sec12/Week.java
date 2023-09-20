2aecae845dc6051fb3f9e0b665ab03c19884f582
soowan95 <ksw9565@gmail.com>
2023-09-20 10:33:34 +0900

    example enum

---
+++ java_study/java20230828/src/ch05Reference/book/ch05/sec12/Week.java
@@ -0,0 +1,11 @@
+package ch05Reference.book.ch05.sec12;
+
+public enum Week {
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY
+}
---
+++ java_study/java20230828/src/ch12Api/book/ch12/sec11/exam01/Car.java
@@ -0,0 +1,4 @@
+package ch12Api.book.ch12.sec11.exam01;
+
+public class Car {
+}

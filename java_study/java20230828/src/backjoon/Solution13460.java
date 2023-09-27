b45f06a39da1c365038ad65f0bfb01bd61b4068f
soowan95 <ksw9565@gmail.com>
2023-09-27 17:32:45 +0900

    backjoon13460

---
+++ java_study/java20230828/src/backjoon/Solution13460.java
@@ -0,0 +1,138 @@
+package backjoon;
+
+import java.util.Arrays;
+
+public class Solution13460 {
+  static private int[] redBall;
+  static private int[] blueBall;
+  static private String[] mazeW;
+
+  public int solution13460(int N, int M, String[] mazeW) {
+    setMazeW(mazeW);
+
+    int[] redBall = new int[2];
+    for (int i = 0; i < N; i++) {
+      if (mazeW[i].indexOf("R") != -1) {
+        redBall[0] = i;
+        redBall[1] = mazeW[i].indexOf("R");
+      }
+    }
+
+    setRedBall(redBall);
+
+    int[] blueBall = new int[2];
+    for (int i = 0; i < N; i++) {
+      if (mazeW[i].indexOf("B") != -1) {
+        blueBall[0] = i;
+        blueBall[1] = mazeW[i].indexOf("B");
+      }
+    }
+
+    setBlueBall(blueBall);
+
+    int[] exit = new int[2];
+    for (int i = 0; i < N; i++) {
+      if (mazeW[i].indexOf("O") != -1) {
+        exit[0] = i;
+        exit[1] = mazeW[i].indexOf("O");
+      }
+    }
+
+    leftB(mazeW, blueBall);
+    rightB(mazeW, blueBall);
+    Arrays.stream(blueBall).forEach(System.out::println);
+
+    String[] turn = turn(mazeW);
+    System.out.println(turn[1]);
+
+    upR(mazeW, redBall);
+    Arrays.stream(redBall).forEach(System.out::println);
+
+    return 0;
+  }
+
+  public void upR(String[] mazeW, int[] locate) {
+    String[] mazeL = turn(mazeW);
+    locate[0] = mazeL[locate[1]].indexOf(".");
+    mazeL[locate[0]] = mazeL[locate[1]].replace("R", ".");
+    mazeL[locate[1]] = mazeL[locate[1]].replaceFirst("\\.", "R");
+    redBall = locate;
+    this.mazeW = turn(mazeL);
+  }
+
+  public void upB(String[] mazeW, int[] locate) {
+    String[] mazeL = turn(mazeW);
+    locate[0] = mazeL[locate[1]].indexOf(".");
+    mazeL[locate[0]] = mazeL[locate[1]].replace("B", ".");
+    mazeL[locate[1]] = mazeL[locate[1]].replaceFirst("\\.", "B");
+    redBall = locate;
+    this.mazeW = turn(mazeL);
+  }
+
+  public void leftR(String[] mazeW, int[] locate) {
+    locate[1] = mazeW[locate[0]].indexOf(".");
+    mazeW[locate[0]] = mazeW[locate[0]].replace("R", ".");
+    mazeW[locate[0]] = mazeW[locate[0]].replaceFirst("\\.", "R");
+    redBall = locate;
+    this.mazeW = mazeW;
+  }
+
+  public void leftB(String[] mazeW, int[] locate) {
+    locate[1] = mazeW[locate[0]].indexOf(".");
+    mazeW[locate[0]] = mazeW[locate[0]].replace("B", ".");
+    mazeW[locate[0]] = mazeW[locate[0]].replaceFirst("\\.", "B");
+    redBall = locate;
+    this.mazeW = mazeW;
+  }
+
+  public void rightR(String[] mazeW, int[] locate) {
+    String rev = revStr(mazeW[locate[0]]);
+    locate[1] = rev.length() - rev.indexOf(".") - 1;
+    rev = rev.replace("R", ".");
+    rev = rev.replaceFirst("\\.", "R");
+    redBall = locate;
+    mazeW[locate[0]] = revStr(rev);
+    this.mazeW = mazeW;
+  }
+
+  public void rightB(String[] mazeW, int[] locate) {
+    String rev = revStr(mazeW[locate[0]]);
+    locate[1] = rev.length() - rev.indexOf(".") - 1;
+    rev = rev.replace("B", ".");
+    rev = rev.replaceFirst("\\.", "B");
+    redBall = locate;
+    mazeW[locate[0]] = revStr(rev);
+    this.mazeW = mazeW;
+  }
+
+  public static void setMazeW(String[] mazeW) {
+    Solution13460.mazeW = mazeW;
+  }
+
+  public static void setRedBall(int[] redBall) {
+    Solution13460.redBall = redBall;
+  }
+
+  public static void setBlueBall(int[] blueBall) {
+    Solution13460.blueBall = blueBall;
+  }
+
+  public static String revStr(String str) {
+    String revStr = "";
+    for (int i = str.length(); i > 0; i--) {
+      revStr += String.valueOf(str.charAt(i - 1));
+    }
+    return revStr;
+  }
+
+  public static String[] turn(String[] mazeW) {
+    String[] turn = new String[mazeW.length];
+
+    for(int i = 0; i < mazeW[0].length(); i++) {
+      final int j = i;
+      turn[i] = Arrays.stream(mazeW).map(a -> String.valueOf(a.charAt(j))).reduce("", String::concat);
+    }
+
+    return turn;
+  }
+}
---
+++ java_study/java20230828/src/backjoon/Solution13460Test.java
@@ -0,0 +1,14 @@
+package backjoon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution13460Test {
+
+  @Test
+  void solution13460() {
+    Solution13460 o1 = new Solution13460();
+    assertEquals(1, o1.solution13460(5, 5, new String[]{"#####", "#..B#", "#.#.#", "#RO.#", "#####"}));
+  }
+}

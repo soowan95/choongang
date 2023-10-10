ca5371c74512b9550c950047f301c5f7190cd698
soowan95 <ksw9565@gmail.com>
2023-10-10 15:14:05 +0900

    backjoon13460

--- java_study/java20230828/src/backjoon/Solution13460.java
+++ java_study/java20230828/src/backjoon/Solution13460.java
@@ -8,6 +8,7 @@ public class Solution13460 {
   static private int[] exitLoca;
   static private String[] global_mazeW;
   static private boolean keep;
+  static private int count;
 
   public int solution13460(int N, int M, String[] mazeW) {
     setKeep(true);
@@ -43,12 +44,15 @@ public class Solution13460 {
 
     setExitLoca(exit);
 
-    leftB(global_mazeW, blueBall);
-    upR(global_mazeW, redBall);
+//    up();
     downR(global_mazeW, redBall);
     downB(global_mazeW, blueBall);
-    rightR(global_mazeW, redBall);
+    right();
+//    leftB(global_mazeW, blueBall);
+//    leftR(global_mazeW, redBall);
+//    down();
 
+    System.out.println("count = " + count);
     Arrays.stream(global_mazeW).forEach(System.out::println);
     System.out.println("빨간공 위치");
     Arrays.stream(redBall).forEach(System.out::println);
@@ -60,100 +64,216 @@ public class Solution13460 {
     return 0;
   }
 
+  public void up() {
+    if (redBall[0] > blueBall[0]) {
+      upR(global_mazeW, redBall);
+      upB(global_mazeW, blueBall);
+    } else {
+      upB(global_mazeW, blueBall);
+      upR(global_mazeW, redBall);
+    }
+  }
+
+  public void down() {
+    if (redBall[0] < blueBall[0]) {
+      downB(global_mazeW, blueBall);
+      downR(global_mazeW, redBall);
+    } else {
+      downR(global_mazeW, redBall);
+      downB(global_mazeW, blueBall);
+    }
+  }
+
+  public void left() {
+    if (redBall[1] < blueBall[1]) {
+      leftR(global_mazeW, redBall);
+      leftB(global_mazeW, blueBall);
+    } else {
+      leftB(global_mazeW, blueBall);
+      leftR(global_mazeW, redBall);
+    }
+  }
+
+  public void right() {
+    if (redBall[1] > blueBall[1]) {
+      rightR(global_mazeW, redBall);
+      rightB(global_mazeW, blueBall);
+    } else {
+      rightB(global_mazeW, blueBall);
+      rightR(global_mazeW, redBall);
+    }
+  }
+
   public void upR(String[] mazeW, int[] locate) {
     mazeW = turn(mazeW);
-    locate[0] = mazeW[locate[1]].indexOf(".");
-    mazeW[locate[1]] = mazeW[locate[1]].replace("R", ".");
-    mazeW[locate[1]] = mazeW[locate[1]].replaceFirst("\\.", "R");
+    int repla = mazeW[locate[1]].length() - mazeW[locate[1]].replaceAll("#\\.+[#BO]+", "").length();
+    String repStr = "";
+    if (repla == 0) repla = mazeW[locate[1]].indexOf(".");
+    for (int i = 0; i < repla; i++) {
+      repStr += "#";
+    }
+    if (mazeW[locate[1]].replaceAll("#\\.+[#BO]+", repStr).contains(".") && mazeW[locate[1]].charAt(mazeW[locate[1]].indexOf("R")-1) == '.') {
+      locate[0] = mazeW[locate[1]].replaceAll("#\\.+[#BO]+", repStr).indexOf(".");
+      mazeW[locate[1]] = mazeW[locate[1]].replace("R", ".");
+      mazeW[locate[1]] = mazeW[locate[1]].substring(0, repla) + "R" + mazeW[locate[1]].substring(repla+1);
+    }
     redBall = locate;
     global_mazeW = turn(mazeW);
 
-    if (redBall[0] - 1 == exitLoca[0]) keep = false;
+    if (redBall[0] - 1 == exitLoca[0] && redBall[1] == exitLoca[1]) keep = false;
+    if (count != -1) count++;
   }
 
   public void downR(String[] mazeW, int[] locate) {
     String[] mazeL = turn(mazeW);
     String rev = revStr(mazeL[locate[1]]);
-    locate[0] = rev.length() - rev.indexOf(".") - 1;
-    rev = rev.replace("R", ".");
-    rev = rev.replaceFirst("\\.", "R");
+    int repla = rev.length() - rev.replaceAll("#\\.+[#BO]+", "").length();
+    if (repla == 0) repla = rev.indexOf(".");
+    String repStr = "";
+    for (int i = 0; i < repla; i++) {
+      repStr += "#";
+    }
+    if (rev.replaceAll("#\\.+[#BO]+", repStr).contains(".") && rev.charAt(rev.indexOf("R")-1) == '.') {
+      locate[0] = rev.length() - rev.replaceAll("#\\.+[#BO]+", repStr).indexOf(".") -1;
+      rev = rev.replace("R", ".");
+      rev = rev.substring(0, repla) + "R" + rev.substring(repla+1);
+    }
     redBall = locate;
     mazeL[locate[1]] = revStr(rev);
     global_mazeW = turn(mazeL);
 
-    if (redBall[0] + 1 == exitLoca[0]) keep = false;
+    if (redBall[0] + 1 == exitLoca[0] && redBall[1] == exitLoca[1]) keep = false;
+    if (count != -1) count++;
   }
 
   public void leftR(String[] mazeW, int[] locate) {
-    int repla = mazeW[locate[0]].length() - mazeW[locate[0]].replaceAll("#\\.+[#B]", "").length();
+    int repla = mazeW[locate[0]].length() - mazeW[locate[0]].replaceAll("#\\.+[#BO]+", "").length();
+    if (repla == 0) repla = mazeW[locate[0]].indexOf(".");
     String repStr = "";
     for (int i = 0; i < repla; i++) {
       repStr += "#";
     }
-    locate[1] = mazeW[locate[0]].replaceAll("#\\.+[#BO]", repStr).indexOf(".");
-    mazeW[locate[0]] = mazeW[locate[0]].replace("R", ".");
-    mazeW[locate[0]] = mazeW[locate[0]].substring(0, repla) + "R" + mazeW[locate[0]].substring(repla);
+    if (mazeW[locate[0]].replaceAll("#\\.+[#BO]+", repStr).contains(".") && mazeW[locate[0]].charAt(mazeW[locate[0]].indexOf("R")-1) == '.') {
+      locate[1] = mazeW[locate[0]].replaceAll("#\\.+[#BO]+", repStr).indexOf(".");
+      mazeW[locate[0]] = mazeW[locate[0]].replace("R", ".");
+      mazeW[locate[0]] = mazeW[locate[0]].substring(0, repla) + "R" + mazeW[locate[0]].substring(repla+1);
+    }
     redBall = locate;
     global_mazeW = mazeW;
 
-    if (redBall[1] - 1 == exitLoca[1]) keep = false;
+    if (redBall[1] - 1 == exitLoca[1] && redBall[0] == exitLoca[0]) keep = false;
+    if (count != -1) count++;
   }
 
   public void rightR(String[] mazeW, int[] locate) {
     String rev = revStr(mazeW[locate[0]]);
-    int repla = rev.length() - rev.replaceAll("#\\.+[#B]", "").length();
+    int repla = rev.length() - rev.replaceAll("#\\.+[#BO]+", "").length();
+    if (repla == 0) repla = rev.indexOf(".");
     String repStr = "";
     for (int i = 0; i < repla; i++) {
       repStr += "#";
     }
-    if(rev.replaceAll("#\\.+[#BO]", repStr).contains(".")) {
-      locate[1] = rev.replaceAll("#\\.+[#B]", repStr).indexOf(".");
+    if(rev.replaceAll("#\\.+[#BO]+", repStr).contains(".") && rev.charAt(rev.indexOf("R")-1) == '.') {
+      locate[1] = rev.length() - rev.replaceAll("#\\.+[#BO]+", repStr).indexOf(".") -1;
       rev = rev.replace("R", ".");
-      rev = rev.substring(0, repla) + "R" + rev.substring(repla);
+      rev = rev.substring(0, repla) + "R" + rev.substring(repla+1);
     }
     redBall = locate;
     mazeW[locate[0]] = revStr(rev);
     global_mazeW = mazeW;
 
-    if (redBall[1] + 1 == exitLoca[1]) keep = false;
+    if (redBall[1] + 1 == exitLoca[1] && redBall[0] == exitLoca[0]) keep = false;
+    if (count != -1) count++;
   }
 
   public void upB(String[] mazeW, int[] locate) {
     mazeW = turn(mazeW);
-    locate[0] = mazeW[locate[1]].indexOf(".");
-    mazeW[locate[1]] = mazeW[locate[1]].replace("B", ".");
-    mazeW[locate[1]] = mazeW[locate[1]].replaceFirst("\\.", "B");
-    blueBall = locate;
+    int repla = mazeW[locate[1]].length() - mazeW[locate[1]].replaceAll("#\\.+[#RO]+", "").length();
+    String repStr = "";
+    if (repla == 0) repla = mazeW[locate[1]].indexOf(".");
+    for (int i = 0; i < repla; i++) {
+      repStr += "#";
+    }
+    if (mazeW[locate[1]].replaceAll("#\\.+[#RO]+", repStr).contains(".") && mazeW[locate[1]].charAt(mazeW[locate[1]].indexOf("B")-1) == '.') {
+      locate[0] = mazeW[locate[1]].replaceAll("#\\.+[#RO]+", repStr).indexOf(".");
+      mazeW[locate[1]] = mazeW[locate[1]].replace("B", ".");
+      mazeW[locate[1]] = mazeW[locate[1]].substring(0, repla) + "B" + mazeW[locate[1]].substring(repla+1);
+    }
+    redBall = locate;
     global_mazeW = turn(mazeW);
+
+    if (redBall[0] - 1 == exitLoca[0] && redBall[1] == exitLoca[1]) {
+      keep = false;
+      count = -1;
+    }
   }
 
   public void downB(String[] mazeW, int[] locate) {
     String[] mazeL = turn(mazeW);
     String rev = revStr(mazeL[locate[1]]);
-    locate[0] = rev.length() - rev.indexOf(".") - 1;
-    rev = rev.replace("B", ".");
-    rev = rev.replaceFirst("\\.", "B");
-    blueBall = locate;
+    int repla = rev.length() - rev.replaceAll("#\\.+[#RO]+", "").length();
+    if (repla == 0) repla = rev.indexOf(".");
+    String repStr = "";
+    for (int i = 0; i < repla; i++) {
+      repStr += "#";
+    }
+    if (rev.replaceAll("#\\.+[#RO]+", repStr).contains(".") && rev.charAt(rev.indexOf("B")-1) == '.') {
+      locate[0] = rev.length() - rev.replaceAll("#\\.+[#RO]+", repStr).indexOf(".") -1;
+      rev = rev.replace("B", ".");
+      rev = rev.substring(0, repla) + "B" + rev.substring(repla+1);
+    }
+    redBall = locate;
     mazeL[locate[1]] = revStr(rev);
     global_mazeW = turn(mazeL);
+
+    if (redBall[0] + 1 == exitLoca[0] && redBall[1] == exitLoca[1]) {
+      keep = false;
+      count = -1;
+    }
   }
 
   public void leftB(String[] mazeW, int[] locate) {
-    locate[1] = mazeW[locate[0]].indexOf(".");
-    mazeW[locate[0]] = mazeW[locate[0]].replace("B", ".");
-    mazeW[locate[0]] = mazeW[locate[0]].replaceFirst("\\.", "B");
-    blueBall = locate;
+    int repla = mazeW[locate[0]].length() - mazeW[locate[0]].replaceAll("#\\.+[#RO]+", "").length();
+    if (repla == 0) repla = mazeW[locate[0]].indexOf(".");
+    String repStr = "";
+    for (int i = 0; i < repla; i++) {
+      repStr += "#";
+    }
+    if (mazeW[locate[0]].replaceAll("#\\.+[#RO]+", repStr).contains(".") && mazeW[locate[0]].charAt(mazeW[locate[0]].indexOf("B")-1) == '.') {
+      locate[1] = mazeW[locate[0]].replaceAll("#\\.+[#RO]+", repStr).indexOf(".");
+      mazeW[locate[0]] = mazeW[locate[0]].replace("B", ".");
+      mazeW[locate[0]] = mazeW[locate[0]].substring(0, repla) + "B" + mazeW[locate[0]].substring(repla+1);
+    }
+    redBall = locate;
     global_mazeW = mazeW;
+
+    if (redBall[1] - 1 == exitLoca[1] && redBall[0] == exitLoca[0]) {
+      keep = false;
+      count = -1;
+    }
   }
 
   public void rightB(String[] mazeW, int[] locate) {
     String rev = revStr(mazeW[locate[0]]);
-    locate[1] = rev.length() - rev.indexOf(".") - 1;
-    rev = rev.replace("B", ".");
-    rev = rev.replaceFirst("\\.", "B");
-    blueBall = locate;
+    int repla = rev.length() - rev.replaceAll("#\\.+[#RO]+", "").length();
+    if (repla == 0) repla = rev.indexOf(".");
+    String repStr = "";
+    for (int i = 0; i < repla; i++) {
+      repStr += "#";
+    }
+    if(rev.replaceAll("#\\.+[#RO]+", repStr).contains(".") && rev.charAt(rev.indexOf("B")-1) == '.') {
+      locate[1] = rev.length() - rev.replaceAll("#\\.+[#RO]+", repStr).indexOf(".") -1;
+      rev = rev.replace("B", ".");
+      rev = rev.substring(0, repla) + "B" + rev.substring(repla+1);
+    }
+    redBall = locate;
     mazeW[locate[0]] = revStr(rev);
     global_mazeW = mazeW;
+
+    if (redBall[1] + 1 == exitLoca[1] && redBall[0] == exitLoca[0]) {
+      keep = false;
+      count = -1;
+    }
   }
 
   public static void setMazeW(String[] mazeW) {
@@ -177,7 +297,7 @@ public class Solution13460 {
   }
 
   public static String[] turn(String[] mazeW) {
-    String[] turn = new String[mazeW.length];
+    String[] turn = new String[mazeW[0].length()];
 
     for(int i = 0; i < mazeW[0].length(); i++) {
       final int j = i;
--- java_study/java20230828/src/backjoon/Solution13460Test.java
+++ java_study/java20230828/src/backjoon/Solution13460Test.java
@@ -9,6 +9,6 @@ class Solution13460Test {
   @Test
   void solution13460() {
     Solution13460 o1 = new Solution13460();
-    assertEquals(1, o1.solution13460(5, 5, new String[]{"#####", "#..B#", "#.#.#", "#RO.#", "#####"}));
+    assertEquals(1, o1.solution13460(5, 5, new String[]{"#######", "#.....#", "#.#B.R#", "#.O...#", "#######"}));
   }
 }
